02c8a1af98da83ab615339c54357d918b8786590
Accute9 <aneeshman07@gmail.com>
2026-05-23 19:06:54 -0400

    cleaned up routes file

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -13,14 +13,6 @@ def index():
 @app.get("/fact-check")
 async def fact_check():
     content = """“Nobody talks about this, but your phone is probably ruining your sleep even if you stop using it before bed. Researchers found that just having your phone in the same room can reduce sleep quality because your brain stays subconsciously alert. That’s why high performers leave their phones outside the bedroom completely. One study even showed people fell asleep faster and had deeper REM sleep after only three nights without a phone nearby.”"""
-    # claim = json.loads(generate_claim(content))["claim"]
-    # # print(type(claim), claim)
-    # separated_claims = json.loads(separate_claim(claim))["subclaims"]
-    # # print(type(separated_claims), separated_claims)
-    # search_results = await asyncio.gather(
-    #     *[asyncio.to_thread(retrieve_sources, cl) for cl in separated_claims]
-    # )
-    # return {"claim": claim, "subclaims": separated_claims, "search_results": search_results,}
     evaluation = evaluate_claim(content)
     return evaluation
 
