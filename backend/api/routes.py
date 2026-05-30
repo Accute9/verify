defeba509a44fae6a5ccae933045ee4744c5d90e
Accute9 <aneeshman07@gmail.com>
2026-05-30 18:45:31 -0400

    untested websocket functionality

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, WebSocket
+from fastapi.responses import HTMLResponse
 from .llm import generate_claim, separate_claim, evaluate_claim
 from .search import retrieve_sources
+from .whisper_model import transcribe_audio
 import json
+import websockets
 import asyncio
+import uuid
 
 app = FastAPI()
 
@@ -16,5 +20,24 @@ async def fact_check():
     evaluation = await evaluate_claim(content)
     return evaluation
 
-    
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_bytes() # some chunk of audio data from chrome.tabCapture
+            with open(f"temp/{uuid.uuid4()}_audio_chunk.wav", "wb") as f:
+                f.write(data)
+            transcription = asyncio.get_event_loop().run_in_executor(None, transcribe_audio, "temp/audio_chunk.wav")
+            return_data = {
+                "transcription": transcription,
+            }
+            await websocket.send_text(json.dumps(return_data))
+    except websockets.expcetions.ConnectionClosedError:
+        print("WebSocket disconnected unexpectedly")
+
+@app.post("/transcribe")
+def transcribe(file: UploadFile = File(...)):
+    file_location = f"temp/{file.filename}" # deleted after transcription
  
+
